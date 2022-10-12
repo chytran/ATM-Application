952e4a9a7a8819d72df5e03b55b82c72bdf917f7
Kevin Tran <kevintran1142@gmail.com>
2022-10-12 11:50:58 -0700

    Add project files.

---
+++ ATM Application/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using ATM_Application.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient;
+
+namespace ATM_Application.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private readonly ILogger<HomeController> _logger;
+
+        /*public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        */
+
+        private readonly IConfiguration configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            string connectionstring = configuration.GetConnectionString("DefaultConnectionString");
+
+            SqlConnection connection = new SqlConnection(connectionstring);
+
+            connection.Open();
+            SqlCommand com = new SqlCommand("Select * from user", connection);
+            var execute = com.ExecuteScalar();
+
+            ViewData["TotalData"] = execute;
+
+            connection.Close();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
